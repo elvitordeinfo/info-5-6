c93e82440dbfcecdaa7979dc7de66a558965191c
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-08-19 20:10:05 +0000

    currency

--- currency.py
+++ currency.py
@@ -1,10 +1,13 @@
 def main():
-    colombia = int(input("Enter what you have left in pesos: "))
-    peru = int(input("Enter what you have left in soles: "))
-    brasil = int(input("Enter what you have left in reais: "))
-    print("You have: ", colombia, "pesos")
-    print("You have: ", peru, "soles")
-    print("You have: ", brasil, "reais")
-    usd = 
+    p = float(input("Enter what you have left in pesos: "))
+    s = float(input("Enter what you have left in soles: "))
+    r = float(input("Enter what you have left in reais: "))
+
+    mxn = (p * 0.0054) + (s* 5.07) + (r * 3.28)
+    usd = round(mxn / 17.06, 2)
+
+    print("USD:", round(usd, 2))
+    print("MXN:", round(mxn, 2))
+
 if __name__ == "__main__":
         main()
