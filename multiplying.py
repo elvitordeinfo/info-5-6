2339fb7f63615e1415e973ba22a616a0b6305172
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-08-20 03:44:00 +0000

    multiplying

---
+++ multiplying.py
@@ -0,0 +1,13 @@
+def main():
+    #The multiplying assigment operator on a string or an integer(which is an asterik before the "=") on python, creates a sequence of repetition. It repeats the specified number of times. Example:
+    text = "Im Victor"
+    text *=20
+    print (text)
+
+    #Another example but with numbers"
+    num = 10
+    num *= 4
+    print(num)
+
+if __name__=="__main__":
+    main()
