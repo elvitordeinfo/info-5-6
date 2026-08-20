6deaf15c614a8e2a54b0b0d352b2a68de080e4e5
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-08-20 20:09:25 +0000

    qr_code

---
+++ qr-code.py
@@ -0,0 +1,13 @@
+import qrcode
+
+def main():
+    song = "https://www.youtube.com/watch?v=C8BTNU_XWhE&list=RDC8BTNU_XWhE&start_radio=1"
+    qr = qrcode.QRcode(version = 1, box_size = 5, border =5)
+    qr.add_data(song)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="blue", back_color="white")
+    img.save("my-qrcode.png")
+
+if __name__=="__main__":
+    main()
