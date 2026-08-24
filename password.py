a4db03041328de8fac7ed0822c0ac29f8f41a0a9
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-08-24 19:35:38 +0000

    password

---
+++ password.py
@@ -0,0 +1,12 @@
+def main():
+    password = "soyelcachorromasguapo"
+
+    attempt = input("Enter your password: ")
+    if attempt == password:
+        print("Correct password.")
+
+    print("End of program.")
+
+
+if __name__ == "__main__":
+    main()
