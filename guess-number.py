404e1636d9f0285306f08b5e8461eaa69a8e0046
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-09-03 20:16:52 +0000

    guess-number

--- guess-number.py
+++ guess-number.py
@@ -1,19 +1,19 @@
 import random
-
 def main():
-    na = input("Hello! What is your name?: ")
-    print("Well" , na , "I am thinking of a number between 1 and 100.")
-    number = random.randint(1,100)
-    answer = int(input("Take a guess..:"))
+    name = input("Hello! What is your name? ")
+    print(f"Well, {name}, I am thinking of a number between 1 and 100.")
 
-    while answer != number:
-        if answer > number:
-            print("Your guess is too high. Take a guess..:")
-        elif answer < number:
-            print("Your guess is to low. Take a guess..:")
-        elif answer == number:
-            print("Good job,", na, "! You guessed my number!")
-            break
+    number = random.randint(1,100)
+    guess = 0
+
+    while guess != number:
+        guess = int(input("Take a guess: "))
+        if guess > number:
+            print("Your guess is too high.")
+        elif guess < number:
+            print("Your guess is too low.")
+
+    print(f"Good job, {name}! You guessed my number!")
 
 if __name__ == "__main__":
     main()
