ae314437b6343023485068e87a75721e087353ec
elvitordeinfo <academiajuarezcomputerlab@gmail.com>
2026-09-04 17:30:38 +0000

    learning

---
+++ learning-app.py
@@ -0,0 +1,14 @@
+import random
+def main():
+    print("national pepper")
+    num1 = random.randint(10,99)
+    num2 = random.randint(10,99)
+    num3 = num1 + num2:
+
+    answer = int(input("What is {num1} + {num2}?: ")
+
+if __name__ == "__main__":
+    main()
+
+
+
